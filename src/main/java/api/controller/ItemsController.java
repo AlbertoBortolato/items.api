e8a90ef419bfe2c25bca6c037bef3b3198e49351
giovanni <giobern86@gmail.com>
2016-04-29 11:48:01 +0200

    Inserito DELETE method

--- src/main/java/api/controller/ItemsController.java
+++ src/main/java/api/controller/ItemsController.java
@@ -2,6 +2,7 @@ package api.controller;
 
 import api.model.Items;
 import api.service.ItemsService;
+import org.omg.CORBA.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
@@ -35,11 +36,28 @@ public class ItemsController {
       method = RequestMethod.POST,
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Items> getItemsPost(@RequestParam Map<String, String> queryParamsMap,
+    public ResponseEntity<Items> addItemPost(@RequestParam Map<String, String> queryParamsMap,
                                               @RequestBody Items.Item item){
         service.items().getItems().add(item);
 
         return new ResponseEntity<Items>(service.items(), null, HttpStatus.OK);
     }
+
+    //curl -X DELETE -H "Content-Type: application/json" http://admin:admin-1@localhost:8080/api/{name}
+    @RequestMapping(value={"/{name}",""},
+    method = RequestMethod.DELETE,
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<?> deleteItem(@RequestParam Map<String,String> queryParamsMap,
+                                        @PathVariable("name") String name){
+        for(Items.Item it: service.items().getItems()){
+            if (it.getName().equals(name)){
+                service.items().getItems().remove(it);
+                return new ResponseEntity<Object>(true,null,HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<Object>(false,null,HttpStatus.NO_CONTENT);
+    }
+
 
 }
