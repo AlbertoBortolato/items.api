8708ff5e549530594aac9ef90f8d187ff3d76e5a
alberto <albertobortolato1994@gmail.com>
2016-04-29 16:53:12 +0200

    Aggiunto la cancellazzione Item

--- src/main/java/api/controller/ItemsController.java
+++ src/main/java/api/controller/ItemsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.inject.Inject;
 import java.util.Date;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by matthew on 28.04.16.
@@ -22,26 +23,37 @@ public class ItemsController {
     @Inject
     ItemsService service;
 
-
-    @RequestMapping(value = {"/", ""},
-    method = RequestMethod.GET,
-    produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Items> getItems(@RequestParam Map<String, String> queryParamsMap){
-
-        return new ResponseEntity<Items>(service.items(), null, HttpStatus.OK);
-    }
-
     @RequestMapping(value = {"/", ""}, method = RequestMethod.POST,
       produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
       consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Items.Item> create(@RequestBody Items.Item item1){
-        if(service.pullItem(item1)){
-            return new ResponseEntity<Items.Item>(item1, null, HttpStatus.CREATED);
-        }
-        else{
-            return new ResponseEntity<Items.Item>(item1, null, HttpStatus.BAD_REQUEST);
-        }
+            return new ResponseEntity<Items.Item>(service.pullItem(item1), null, HttpStatus.CREATED);
     }
 
+    @RequestMapping(value = {"/", ""},
+      method = RequestMethod.GET,
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Items> getNome(@RequestParam(value = "query",required = false) String query){
+        if (query != null) {
+            System.out.print("ok");
+            return new ResponseEntity<Items>(service.getName(query), null, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<Items>(service.items(), null, HttpStatus.OK);
+        }
+
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE,
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Items> delete(@PathVariable("id") String id){
+        Items deletato= service.getId(id);
+        if(service.deleteItem(id)){
+            return  new ResponseEntity<Items>(deletato, null, HttpStatus.ACCEPTED);
+        }
+        else{
+            return  new ResponseEntity<Items>(null,null,HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
--- src/main/java/api/service/ItemsService.java
+++ src/main/java/api/service/ItemsService.java
@@ -2,6 +2,7 @@ package api.service;
 
 import api.model.Items;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
@@ -36,23 +37,55 @@ public class ItemsService {
         filePath = Paths.get(file);
     }
 
-    public boolean pullItem(Items.Item item){
-        if(!items.getItems().contains(item)){
+    public Items.Item pullItem(Items.Item item){
             item.setCreationDate(new Date());
             items.getItems().add(item);
+            return item;
+    }
+
+    public Items getName(String name ){
+        Items response= new Items();
+        response.setItems(this.items().getItems().stream()
+          .filter(item -> item.getName() != null)
+          .filter(item -> item.getName().toLowerCase().equals(name.toLowerCase()))
+          .collect(Collectors.toSet()));
+        return response;
+    }
+
+    public Items getId(String id ){
+        Items response= new Items();
+        response.setItems(this.items().getItems().stream()
+          .filter(item -> item.getId() != null)
+          .filter(item -> item.getId().equals(id))
+          .collect(Collectors.toSet()));
+        return response;
+    }
+
+
+    public  void setItem (Items.Item item1,String id){
+        java.util.Optional<Items.Item> found = this.items().getItems().stream()
+          .filter(item -> item.getId().equals(id))
+          .findFirst();
+        if(!found.equals(java.util.Optional.empty())){
+            this.items.getItems().remove(found);
+            item1.setCreationDate(found.get().getCreationDate());
+            item1.setId(found.get().getId());
+            item1.setModifiedDate(new Date());
+            this.items.getItems().add(item1);
+        }
+    }
+
+    public boolean deleteItem (String id){
+        java.util.Optional<Items.Item> found = this.items().getItems().stream()
+          .filter(item -> item.getId().equals(id))
+          .findFirst();
+        if(!found.equals(java.util.Optional.empty())){
+            this.items.getItems().remove(found.get());
             return true;
         }
         else{
             return false;
         }
-    }
-
-    public Set<Items.Item> getName(String name ){
-        Set<Items.Item> response = this.items().getItems().stream()
-          .filter(item -> item.getName() != null)
-          .filter(item -> item.getName().toLowerCase().equals(name.toLowerCase()))
-          .collect(Collectors.toSet());
-        return response;
     }
 
     public void save(Items items) {
