da03f9687402002a4768aaff0dc77663081b92ca
leon <leon@ekr-proxy>
2016-04-29 17:04:00 +0200

    save prov

--- src/main/java/api/service/ItemsService.java
+++ src/main/java/api/service/ItemsService.java
@@ -2,17 +2,19 @@ package api.service;
 
 import api.model.Items;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Created by matthew on 28.04.16.
@@ -47,7 +49,7 @@ public class ItemsService {
           .filter(item -> item.getId() != null)
           .filter(item -> item.getId().equals(id))
           .findFirst();
-        if(!response.equals(Optional.empty())){
+        if (!response.equals(Optional.empty())) {
             this.items().getItems().remove(response.get());
             b = true;
         }
@@ -61,11 +63,11 @@ public class ItemsService {
           .filter(item -> item.getId() != null)
           .filter(item -> item.getId().equals(id))
           .findFirst();
-        if(!response.equals(Optional.empty()))  {
+        if (!response.equals(Optional.empty())) {
             this.items().getItems().remove(response.get());
             this.items.getItems().add(new_item);
             b = true;
-        }else{
+        } else {
             b = false;
         }
 
@@ -73,8 +75,24 @@ public class ItemsService {
     }
 
 
-    public void save(Items.Item items) {
-        //write filePath
+    public void save(Items.Item itemToSave, Path p) {
+        if(Files.exists(p)) {
+            FileOutputStream fosOldWay = null;
+            try{
+                fosOldWay = new FileOutputStream(p.toFile());
+                fosOldWay.write(itemToSave.getContent(), p);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }finally {
+                IOUtils.closeQuietly(fosOldWay);
+            }
+            try(FileOutputStream fos = new FileOutputStream(p.toFile())) {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ;
+        }
     }
 
     public Items items() {
