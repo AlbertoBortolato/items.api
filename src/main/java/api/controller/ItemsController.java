8e0da33ea965b85821dc59d458f996fb96b7ae6a
giovanni <giobern86@gmail.com>
2016-04-29 14:31:37 +0200

    modificati metodi

--- src/main/java/api/controller/ItemsController.java
+++ src/main/java/api/controller/ItemsController.java
@@ -2,6 +2,7 @@ package api.controller;
 
 import api.model.Items;
 import api.service.ItemsService;
+import javafx.scene.input.InputMethodTextRun;
 import org.omg.CORBA.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -45,35 +46,49 @@ public class ItemsController {
         return new ResponseEntity<Items>(service.items(), null, HttpStatus.CONFLICT);
     }
 
-    //curl -X DELETE -H "Content-Type: application/json" http://admin:admin-1@localhost:8080/api/{name}
-    @RequestMapping(value = {"/{name}", ""},
+    //curl -X DELETE -H "Content-Type: application/json" http://admin:admin-1@localhost:8080/api/{id}
+    @RequestMapping(value = {"/{id}", ""},
       method = RequestMethod.DELETE,
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> deleteItem(@RequestParam Map<String, String> queryParamsMap,
-                                        @PathVariable("name") String name) {
-        for (Items.Item it : service.items().getItems()) {
+                                        @PathVariable("id") String id) {
+        /*for (Items.Item it : service.items().getItems()) {
             if (it.getName().equals(name)) {
                 service.items().getItems().remove(it);
                 return new ResponseEntity<Object>(true, null, HttpStatus.OK);
             }
+        }*/
+        Items.Item item = new Items.Item();
+        item.setId(id);
+        if (service.items().getItems().contains(item.getId())){
+            service.items().getItems().remove(item);
+            return new ResponseEntity<Object>(service.items(), null, HttpStatus.OK);
         }
-        return new ResponseEntity<Object>(false, null, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<Object>(service.items(), null, HttpStatus.NO_CONTENT);
     }
 
-    @RequestMapping(value = {"/{name}", ""},
+    //curl -X PUT -H "Content-Type: application/json" -d @test.txt http://admin:admin-1@localhost:8080/api/{id}
+    @RequestMapping(value = {"/{id}", ""},
       method = RequestMethod.PUT,
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<?> updateItem(@RequestParam Map<String, String> queryParamsMap,
-                                        @PathVariable("name") String name,
+                                        @PathVariable("id") String id,
                                         @RequestBody Items.Item item) {
 
-        for (Items.Item it : service.items().getItems()) {
+        /*for (Items.Item it : service.items().getItems()) {
             if (it.getName().equals(name)) {
                 it.setName(item.getName());
                 return new ResponseEntity<Object>(service.items(), null, HttpStatus.OK);
             }
+        }*/
+        Items.Item itemNW = new Items.Item();
+        itemNW.setId(id);
+        if(service.items().getItems().contains(itemNW)) {
+            service.items().getItems().remove(itemNW);
+            service.items().getItems().add(item);
+            return new ResponseEntity<Object>(service.items(), null, HttpStatus.OK);
         }
 
         return new ResponseEntity<Object>(service.items(), null, HttpStatus.NOT_FOUND);
