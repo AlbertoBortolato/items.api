3f7b136655235266c000d4165cb05747467007c0
alberto <albertobortolato1994@gmail.com>
2016-04-29 12:14:02 +0200

    Aggiunto la creazione item

--- src/main/java/api/controller/ItemsController.java
+++ src/main/java/api/controller/ItemsController.java
@@ -6,12 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -24,12 +22,25 @@ public class ItemsController {
     @Inject
     ItemsService service;
 
+
     @RequestMapping(value = {"/", ""},
     method = RequestMethod.GET,
     produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Items> getItems(@RequestParam Map<String, String> queryParamsMap){
 
         return new ResponseEntity<Items>(service.items(), null, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = {"/", ""}, method = RequestMethod.POST,
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+      consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Items.Item> create(@RequestBody Items.Item item1){
+        if(service.pullItem(item1)){
+            return new ResponseEntity<Items.Item>(item1, null, HttpStatus.CREATED);
+        }
+        else{
+            return new ResponseEntity<Items.Item>(item1, null, HttpStatus.BAD_REQUEST);
+        }
     }
 
 
