d9b7d2a3d4f9785a4b606a235d7b7e97dfeca61a
Matthew Bednarski <matthew.bednarski@solinfo.it>
2016-04-29 14:36:47 +0200

    aggiunto logica di serializzazione su file

--- src/main/java/api/config/BeansConfig.java
+++ src/main/java/api/config/BeansConfig.java
@@ -1,10 +1,20 @@
 package api.config;
 
 import api.model.Items;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.inject.Inject;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,14 +23,35 @@ import java.nio.file.Paths;
  */
 @Configuration
 public class BeansConfig {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${api.items.file}")
     String file;
 
+    @Inject
+    ObjectMapper mapper;
+
     @Bean
     public Items items() {
         Path filePath = Paths.get(file);
-
+        if(Files.exists(filePath)) {
+            String sItems = null;
+            try (FileInputStream fis = new FileInputStream(filePath.toFile())) {
+                sItems = IOUtils.toString(fis, "UTF-8");
+            } catch (FileNotFoundException e) {
+                logger.error(e.getLocalizedMessage(), e);
+            } catch (IOException e) {
+                logger.error(e.getLocalizedMessage(), e);
+            }
+            if(!StringUtils.isBlank(sItems)){
+                try {
+                    Items out = mapper.readValue(sItems, Items.class);
+                    return out;
+                } catch (IOException e) {
+                    logger.error(e.getLocalizedMessage(), e);
+                }
+            }
+        }
         //read filePath
         return new Items();
     }
